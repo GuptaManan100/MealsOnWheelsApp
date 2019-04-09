678f8c187316fde1bb69ba4d85f43d1e7d5460eb
GuptaManan100 <guptamanan100@gmail.com>
2019-04-09 20:24:48 +0530

    Login and Signup now successfully working

--- MealsOnWheels/app/src/main/java/com/example/mealsonwheels/MainActivity.java
+++ MealsOnWheels/app/src/main/java/com/example/mealsonwheels/MainActivity.java
@@ -19,7 +19,11 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskCompletionSource;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -29,6 +33,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
@@ -47,6 +52,15 @@ public class MainActivity extends AppCompatActivity {
     private DatabaseReference myRefVendor;
     private DatabaseReference myRefDeliverer;
     private String emailId;
+
+    private TaskCompletionSource<DataSnapshot> userSource = new TaskCompletionSource<>();
+    private Task userTask = userSource.getTask();
+    private TaskCompletionSource<DataSnapshot> venSource = new TaskCompletionSource<>();
+    private Task vendorTask = venSource.getTask();
+    private TaskCompletionSource<DataSnapshot> delivererSource = new TaskCompletionSource<>();
+    private Task delivererTask = delivererSource.getTask();
+
+    private Task<Void> allTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -129,6 +143,7 @@ public class MainActivity extends AppCompatActivity {
                             updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
+                            mGoogleSignInClient.signOut();
                             signInButton.setEnabled(true);
                             Log.w("Login", "signInWithCredential:failure", task.getException());
                             Toast.makeText(MainActivity.this, "Login Failure", Toast.LENGTH_LONG).show();
@@ -142,15 +157,106 @@ public class MainActivity extends AppCompatActivity {
 
     private void updateUI(FirebaseUser user) {
         //All children Access
-        checkIfUserExists(user);
 
-        Toast.makeText(this, "You are not yet registered!", Toast.LENGTH_SHORT).show();
-        Intent mainIntent = new Intent(MainActivity.this, userSignup.class);
-        startActivity(mainIntent);
-        finish();
-        signInButton.setEnabled(true);
+        checkIfUserExists(user);
+        checkIfDelivererExists(user);
+        checkIfVendorExists(user);
+
+        allTask = Tasks.whenAll(userTask, vendorTask, delivererTask);
+        allTask.addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                if(!isFinishing()) {
+                    Toast.makeText(MainActivity.this, "You are not yet registered!", Toast.LENGTH_SHORT).show();
+                    Intent mainIntent = new Intent(MainActivity.this, userSignup.class);
+                    startActivity(mainIntent);
+                    finish();
+                }
+            }
+        });
+        allTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, "Sign In Failed", Toast.LENGTH_SHORT).show();
+                signInButton.setEnabled(true);
+            }
+        });
     }
 
+    private void checkIfUserExists(FirebaseUser user)
+    {
+        emailId = user.getEmail();
+        Query query = myRefUser.orderByChild("email").equalTo(emailId);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot child : dataSnapshot.getChildren())
+                {
+                    Toast.makeText(MainActivity.this, "Login Successfull!!", Toast.LENGTH_LONG).show();
+                    Intent mainIntent = new Intent(MainActivity.this, userHomePage.class);
+                    startActivity(mainIntent);
+                    finish();
+                }
+                userSource.setResult(dataSnapshot);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+                userSource.setException(databaseError.toException());
+            }
+        });
+    }
+
+    private void checkIfVendorExists(FirebaseUser user) {
+        emailId = user.getEmail();
+        Query query1 = myRefVendor.orderByChild("email").equalTo(emailId);
+        query1.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    Toast.makeText(MainActivity.this, "Login Successfull!!", Toast.LENGTH_LONG).show();
+                    Intent mainIntent = new Intent(MainActivity.this, vendorHomePage.class);
+                    startActivity(mainIntent);
+                    finish();
+                }
+                venSource.setResult(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+                venSource.setException(databaseError.toException());
+            }
+        });
+    }
+
+    private void checkIfDelivererExists(FirebaseUser user) {
+        emailId = user.getEmail();
+        Query query = myRefDeliverer.orderByChild("email").equalTo(emailId);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    Toast.makeText(MainActivity.this, "Login Successfull!!", Toast.LENGTH_LONG).show();
+                    Intent mainIntent = new Intent(MainActivity.this, deliveryHomePage.class);
+                    startActivity(mainIntent);
+                    finish();
+                }
+                delivererSource.setResult(dataSnapshot);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+                delivererSource.setException(databaseError.toException());
+            }
+        });
+    }
+
+
+
+
+    //All table access
+    /*
     private void checkIfUserExists(FirebaseUser user) {
         emailId = user.getEmail();
         myRefUser.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -220,4 +326,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    */
 }
